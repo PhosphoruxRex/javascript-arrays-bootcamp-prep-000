85eab70e8d340d8be15d6dbec555829b0d79ddfd
Lung Chen <lungheichen@gmail.com>
2018-06-16 15:47:00 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,1 +1,10 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
+
+function addElementToBeginningOfArray(arr, ele) {
+  arr = arr.unshift(ele)
+  return arr
+}
+
+function destructivelyAddElementToBeginningOfArray(arr, ele) {
+  return arr.unshift(ele)
+}
